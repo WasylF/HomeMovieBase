0f262c42c5ff5e78e19282c713a74fe358bf4092
Vasyl Franchuk <WasylFranchuk@gmail.com>
2016-07-07 23:23:11 +0300

    add new constants

--- src/main/java/wslf/homemoviebase/logic/Constants.java
+++ src/main/java/wslf/homemoviebase/logic/Constants.java
@@ -14,5 +14,16 @@ public class Constants {
      * Maximum files size to load whole it to the RAM. in bytes
      */
     public static final long MAX_SIZE_IN_RAM = 256 * 1024 * 1024; // 256 MB
-
+    /**
+     * message for success status
+     */
+    public static final String SUCCESS_MESSAGE = "OK!100%";
+    /**
+     * minimum legth of caption
+     */
+    public static final int MIN_CAPTION_SIZE = 5;
+    /**
+     * deafault name of root folder
+     */
+    public static final String DEFAULT_ROOT_FOLDER = "HMB";
 }
