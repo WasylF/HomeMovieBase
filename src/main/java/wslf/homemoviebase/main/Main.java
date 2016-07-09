31a936ee104e99e5ebe89769d66af3901a9dd872
Vasyl Franchuk <WasylFranchuk@gmail.com>
2016-07-10 00:41:44 +0300

    configure project main class

--- src/main/java/wslf/homemoviebase/main/Main.java
+++ src/main/java/wslf/homemoviebase/main/Main.java
@@ -8,14 +8,14 @@ import wslf.homemoviebase.logic.Worker;
  * @author Wsl_F
  */
 public class Main {
-    
+
     public static void main(String[] args) {
-        
+
         java.awt.EventQueue.invokeLater(() -> {
             Worker worker = new Worker();
             MainForm mainForm = new MainForm(worker);
             mainForm.setVisible(true);
         });
-        
+
     }
 }
