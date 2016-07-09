ae27955e4b640f2d4f2ef4daee3a51a9384601fa
Vasyl Franchuk <WasylFranchuk@gmail.com>
2016-07-10 00:38:09 +0300

    Create Main class

---
+++ src/main/java/wslf/homemoviebase/main/Main.java
@@ -0,0 +1,21 @@
+package wslf.homemoviebase.main;
+
+import wslf.homemoviebase.forms.MainForm;
+import wslf.homemoviebase.logic.Worker;
+
+/**
+ *
+ * @author Wsl_F
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        java.awt.EventQueue.invokeLater(() -> {
+            Worker worker = new Worker();
+            MainForm mainForm = new MainForm(worker);
+            mainForm.setVisible(true);
+        });
+        
+    }
+}
